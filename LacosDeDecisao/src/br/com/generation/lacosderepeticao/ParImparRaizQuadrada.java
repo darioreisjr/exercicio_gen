d86ae2fa34cebc650cf6d579407e7b9a0f5d6a23
Dario Reis <guttyerresjunior1995@gmail.com>
2022-06-08 17:58:43 -0300

    adiciona atualiza programa

--- LacosDeDecisao/src/br/com/generation/lacosderepeticao/ParImparRaizQuadrada.java
+++ LacosDeDecisao/src/br/com/generation/lacosderepeticao/ParImparRaizQuadrada.java
@@ -1,5 +1,9 @@
 package br.com.generation.lacosderepeticao;
 
+// Faça um programa em que permita a entrada de um número qualquer e exiba se este
+// número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for
+// ímpar exiba o número elevado ao quadrado.
+
 import java.util.Scanner;
 
 public class ParImparRaizQuadrada {
